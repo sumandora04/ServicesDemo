6d7d594054467a18d37b318cdb1ca603b00390f4
SumanDora <suman@jackfruitsoftwarelab.com>
2019-10-21 18:03:14 +0530

    Local Binding

--- app/src/main/java/com/notepoint4u/servicesdemo/MainActivity.java
+++ app/src/main/java/com/notepoint4u/servicesdemo/MainActivity.java
@@ -1,15 +1,26 @@
 package com.notepoint4u.servicesdemo;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Intent serviceIntent;
     private static final String TAG = "Thread";
+    TextView randomText;
+
+    private MyServices myServices;
+    private boolean isServiceBound;
+    private ServiceConnection serviceConnection;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate:Thread id: "+Thread.currentThread().getId());
 
+        randomText = findViewById(R.id.random_number_textView);
     }
 
     public void startServiceClick(View view) {
@@ -26,5 +38,58 @@ public class MainActivity extends AppCompatActivity {
 
     public void stopServiceClick(View view) {
         stopService(serviceIntent);
+    }
+
+    public void getRandomNumberClick(View view) {
+        if (isServiceBound) {
+            randomText.setText(String.valueOf(myServices.getRandomNumber()));
+        }else {
+            randomText.setText("Service is not bound.");
+        }
+    }
+
+    public void unbindServiceClick(View view) {
+        unBindServiceMethod();
+    }
+
+    public void bindServiceClick(View view) {
+        bindServiceMethod();
+    }
+
+
+    private void bindServiceMethod(){
+        if(serviceConnection==null){
+            serviceConnection = new ServiceConnection() {
+                @Override
+                public void onServiceConnected(ComponentName name, IBinder iBinder) {
+
+                    MyServices.MyServiceBinder myServiceBinder = (MyServices.MyServiceBinder)iBinder ;
+                    myServices = myServiceBinder.getBinder();
+
+                    isServiceBound = true;
+                }
+
+                @Override
+                public void onServiceDisconnected(ComponentName name) {
+
+                    isServiceBound = false;
+                }
+            };
+        }
+
+        bindService(serviceIntent,serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private void unBindServiceMethod(){
+        if (isServiceBound){
+            unbindService(serviceConnection);
+            isServiceBound = false;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unBindServiceMethod();
     }
 }
--- app/src/main/java/com/notepoint4u/servicesdemo/MyServices.java
+++ app/src/main/java/com/notepoint4u/servicesdemo/MyServices.java
@@ -2,29 +2,51 @@ package com.notepoint4u.servicesdemo;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import java.util.Random;
+
 public class MyServices extends Service {
     private static final String TAG = "Thread";
 
-    //Deprecated
+    private int mRandomNumber;
+    private boolean mIsRandomGeneratorOn;
+    private final int MIN = 0;
+    private final int MAX = 100;
+
+//    Deprecated
 //    @Override
 //    public void onStart(Intent intent, int startId) {
 //        super.onStart(intent, startId);
 //    }
 
+    //To get the IBinder instance:
+    //1. create a class and extend the Binder
+    //2. Create an instance of IBinder
+    //3. Return the IBinder instance in onBind()
+    public class MyServiceBinder extends Binder{
+        public MyServices getBinder(){
+            return MyServices.this;
+        }
+    }
+
+    private IBinder mIBinder = new MyServiceBinder();
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        Log.d(TAG, "onBind");
+        return mIBinder;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: Service destroyed");
+        stopRandomNumberGenerator();
     }
 
     @Override
@@ -32,6 +54,38 @@ public class MyServices extends Service {
         Log.d(TAG, "onStartCommand: Thread id:"+Thread.currentThread().getId());
         //To stop the service when it is done with the work.--- automatically stop
        // stopSelf();
+
+        mIsRandomGeneratorOn = true;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                onStartRandomNumberGenerator();
+            }
+        }).start();
+
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void onStartRandomNumberGenerator(){
+        while (mIsRandomGeneratorOn){
+            try {
+                Thread.sleep(1000);
+                if (mIsRandomGeneratorOn){
+                    mRandomNumber = new Random().nextInt(MAX)+MIN;
+                    Log.d(TAG, "Thread id: "+Thread.currentThread().getId()+", Random Number: "+ mRandomNumber);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void stopRandomNumberGenerator(){
+        mIsRandomGeneratorOn =false;
+    }
+
+    public int getRandomNumber(){
+        return mRandomNumber;
     }
 }
